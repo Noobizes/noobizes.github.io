efbc2bde4d06fc40f32228bc692792b28c413ea1
LG <lgiffard44@gmail.com>
2022-12-08 18:00:07 +0100

    main page proposition

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,6 +19,82 @@ export default function Home(): JSX.Element {
             We try to be transparent as possible, if any information are missing
             feel free to contact us.
           </h2>
+          <div className="home-buttons">
+            <button
+              className="home-buttons__button"
+              onClick={() => window.open('https://noobizes.com', '_blank')}
+            >
+              <svg
+                className="home-buttons__button-icon"
+                width="18"
+                height="16"
+                viewBox="0 0 18 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M17.3047 8.92969L10.8203 15.8047C10.7031 15.9609 10.5078 16 10.3516 16C10.1953 16 10.0391 15.9609 9.92188 15.8438C9.64844 15.6094 9.64844 15.2188 9.88281 14.9844L15.3516 9.125H0.625C0.273438 9.125 0 8.85156 0 8.53906C0 8.22656 0.273438 7.875 0.625 7.875H15.3516L9.88281 2.05469C9.64844 1.82031 9.64844 1.42969 9.92188 1.19531C10.1953 0.960938 10.5859 0.960938 10.8203 1.23438L17.3047 8.10938C17.5391 8.34375 17.5391 8.69531 17.3047 8.92969Z"
+                  fill="#202227"
+                />
+              </svg>
+              <span className="home-buttons__button-text">
+                Discover the project
+              </span>
+            </button>
+          </div>
+        </div>
+        <div className="home-categories">
+          <h3 className="home-categories__title">Do Your Own Research !</h3>
+          <div className="home-categories__container">
+            <div
+              className="home-categories__card"
+              style={{
+                background: 'url(img/token-background.jpg) center',
+                backgroundSize: '100%',
+              }}
+              onClick={() => window.open('/docs/platform', '_self')}
+            >
+              <span className="home-categories__card-text">
+                Discover Noobizes Platform
+              </span>
+            </div>
+            <div
+              className="home-categories__card"
+              style={{
+                background: 'url(img/token-background.jpg) center',
+                backgroundSize: '100%',
+              }}
+              onClick={() => window.open('/docs/team', '_self')}
+            >
+              <span className="home-categories__card-text">
+                Get to know the team
+              </span>
+            </div>
+            <div
+              className="home-categories__card"
+              style={{
+                background: 'url(img/token-background.jpg) center',
+                backgroundSize: '100%',
+              }}
+              onClick={() => window.open('/docs/og', '_self')}
+            >
+              <span className="home-categories__card-text">
+                Want to be more involved ?
+              </span>
+            </div>
+            <div
+              className="home-categories__card"
+              style={{
+                background: 'url(img/token-background.jpg) center',
+                backgroundSize: '100%',
+              }}
+              onClick={() => window.open('/docs/tokenomic', '_self')}
+            >
+              <span className="home-categories__card-text">
+                Understand the economic model
+              </span>
+            </div>
+          </div>
         </div>
       </main>
     </Layout>

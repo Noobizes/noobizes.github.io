121fadfaf14854716dad53922b30176865aa33ef
LG <lgiffard44@gmail.com>
2023-01-10 18:14:12 +0100

    remove blog since no article

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -54,7 +54,7 @@ const config = {
             position: 'left',
             label: 'Documentation',
           },
-          { to: 'blog', label: 'Blog', position: 'left' },
+          //{ to: 'blog', label: 'Blog', position: 'left' },
         ],
       },
       footer: {
